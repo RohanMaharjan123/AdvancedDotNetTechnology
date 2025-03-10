4132681371c33283fb702d886a013bdea694c75f
Rohan Maharjan <nahor.uzamaki@gmail.com>
2025-03-10 10:35:00 +0545

    added a C# program that implements multi-threading and thread synchronization

---
+++ src/Lab/Lab02/Program.cs
@@ -0,0 +1,26 @@
+﻿class Program
+{
+    static object lockObject = new object();
+
+    static void Main(string[] args)
+    {
+        Thread thread1 = new Thread(PrintNumbers!);
+        Thread thread2 = new Thread(PrintNumbers!);
+
+        thread1.Start("Thread 1");
+        thread2.Start("Thread 2");
+    }
+
+    static void PrintNumbers(object threadName)
+    {
+        lock (lockObject)
+        {
+            for(int i = 1; i <= 5; i++)
+            {
+                Console.WriteLine($"{threadName}: {i}");
+
+                Thread.Sleep(100);
+            }
+        }
+    }
+}
